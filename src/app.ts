36e21a8501969cd82dcc3e6027746ab48dcefb17
Ben Chan <bchan158@my.bcit.ca>
2025-10-05 22:56:21 -0700

    Update CORS

--- src/app.ts
+++ src/app.ts
@@ -13,18 +13,7 @@ import { initializeCache } from "./controllers/flashcardController";
 export const app = new Hono();
 
 app.use("*", logger());
-app.use(
-  "/*",
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:8080",
-      "https://frontend-cl3c.onrender.com/",
-      "https://backend-84zo.onrender.com/",
-    ],
-    credentials: true,
-  })
-);
+app.use("/*", cors());
 
 app.route("/", homeRoute);
 app.route("/chat", chatRoute);
