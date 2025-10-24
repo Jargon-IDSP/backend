c428061de5bdf9e40b71bfdb59234d7e8f94361d
Dilraj <djrandhawa15@hotmail.com>
2025-10-23 18:27:42 -0700

    added chatbot

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import "dotenv/config";
 import { serve } from "@hono/node-server";
 import { app } from "./app";
+
 
 const port = +(process.env.PORT || 8080);
 
@@ -7,5 +9,12 @@ serve({
   port,
   fetch: app.fetch,
 });
+console.log("ENV CHECK", {
+  cwd: process.cwd(),
+  hasPK: !!process.env.CLERK_PUBLISHABLE_KEY,
+  hasSK: !!process.env.CLERK_SECRET_KEY,
+  hasGemini: !!process.env.GOOGLE_GENAI_API_KEY,
+});
+
 
 console.log(`Server running on http://localhost:${port}`);
